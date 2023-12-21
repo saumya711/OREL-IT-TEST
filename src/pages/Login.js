87b54c7a4089e61efc92d50ce6c3d68ae8d73886
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-12-22 00:13:54 +0530

    CHG | Login Form UI

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,18 +1,27 @@
 import React, { useState, useEffect} from 'react';
 import { toast } from 'react-toastify';
 import { GoogleOutlined, MailOutlined } from '@ant-design/icons';
-import { Button } from 'antd';
+import { Button, Card } from 'antd';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
 
-  const [email, setEmail] = useState("niroshanikumari758@gmail.com");
-  const [password, setPassword] = useState("123456");
+  const inputStyle = {
+    border: '1px solid #ccc',
+    outline: 'none',
+    padding: '8px',
+    // Add any other styles as needed
+  };
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
 
   const loginForm = () => {
     return(
       <form >
         <div className='form-group'>
+          <label>Email</label>
           <input 
           type='email' 
           className='form-control'
@@ -22,10 +31,11 @@ const Login = () => {
           autoFocus />
         </div>
       
-        <div className='form-group'>
+        <div className='form-group mt-3'>
+          <label>Password</label>
           <input 
           type='password' 
-          className='form-control mt-3'
+          className='form-control'
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           placeholder='Your password'
@@ -33,45 +43,45 @@ const Login = () => {
         </div>
 
         <Button
-        //onClick={handleSubmit}
-        type='primary'
-        className='mb-3'
-        block
-        shape='round'
-        icon={<MailOutlined/>}
-        size='large'
-        disabled={!email || password.length < 6}>
-          Login with Email/Password
+          //onClick={handleSubmit}
+          className='mb-3 mt-3'
+          block
+          size='large'
+          disabled={!email || password.length < 6}
+          style={{ backgroundColor: 'red', borderColor: 'red', color: 'white' }}
+        >
+          Log in
         </Button>
       </form>
     )
     
   }
   return (
-    <div className="container p-5">
-      <div className="row">
-        <div className="col-md-6 offset-md-">
-          {loading ? (
-            <h4 className='text-danger'>Loading...</h4>
-            ) : (
-              <h4>Login</h4>
-            )}
-          {loginForm()}
-
-          <Button
-            //onClick={googleLogin}
-            type='secondary'
-            className='mb-3'
-            block
-            shape='round'
-            icon={<GoogleOutlined/>}
-            size='large'
-          >
-            Login with Google
-          </Button>
-        </div>
+    <Card className="col-md-4 offset-md-3 mx-auto p-5">
+      {loading ? (
+        <h4 className='text-danger text-center'>Loading...</h4>
+        ) : (
+          <>
+            <h4 className='text-bold text-center'>OrelBuy</h4>
+            <p className='text-bold text-center'>Hello, Welcome to OrelBuy</p>
+          </>
+        )}
+      {loginForm()}
+      <div className='text-center mb-3'>
+        <p>
+          Don't have and account?{' '}
+          <span>
+            <Link>Sign up</Link>
+          </span>
+        </p>
+        <p>
+          Forgot your password?{' '}
+          <span>
+            <Link>Reset it</Link>
+          </span>
+        </p>
       </div>
-    </div>
+    </Card>
   );
 }
 
